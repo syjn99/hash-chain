99645f19bd31325402ef5ddc5d0535b37c74e917
Jun Song <syjn99@gmail.com>
2025-04-02 22:07:59 +0900

    feat: add pickle for finding big k

--- common.py
+++ common.py
@@ -1,5 +1,10 @@
 import hashlib
+import os
 from collections.abc import Callable
+import pickle
+from typing import Tuple
+
+PICKLES = "pickles"
 
 
 def make_hash_function(k: int) -> Callable[[bytes], bytes]:
@@ -9,3 +14,21 @@ def make_hash_function(k: int) -> Callable[[bytes], bytes]:
         return hashed[-byte:]
 
     return hash_function
+
+
+def save_checkpoint(filename, t: bytes, h: bytes, count: int):
+    with open(filename, "wb") as f:
+        pickle.dump((t, h, count), f)
+
+
+def load_checkpoint(filename) -> Tuple[bytes, bytes, int]:
+    with open(filename, "rb") as f:
+        return pickle.load(f)
+
+
+def delete_checkpoint(filename: str):
+    if os.path.exists(filename):
+        os.remove(filename)
+        print(f"Deleted checkpoint file: {filename}")
+    else:
+        print(f"No checkpoint file found at: {filename}")
--- cycle.py
+++ cycle.py
@@ -1,6 +1,18 @@
 import time
+from collections.abc import Callable
+from typing import Tuple
 
-from common import make_hash_function
+from common import make_hash_function, save_checkpoint, load_checkpoint, delete_checkpoint, PICKLES
+
+# For every `CHECKPOINT` iteration, save to pickle file.
+CHECKPOINT = 10_000_000
+
+
+STEP1_PKL = f"{PICKLES}/cycle-step1.pkl"
+STEP1_COMPLETE_PKL = f"{PICKLES}/cycle-step1-complete.pkl"
+STEP2_PKL = f"{PICKLES}/cycle-step2.pkl"
+STEP2_COMPLETE_PKL = f"{PICKLES}/cycle-step2-complete.pkl"
+STEP3_PKL = f"{PICKLES}/cycle-step3.pkl"
 
 
 def find_cycle(k: int, iv: str):
@@ -15,28 +27,123 @@ def find_cycle(k: int, iv: str):
     # `t` for Tortoise, `h` for Hare.
     # `t` jumps one edge per step while `f` jumps two edges per step.
 
-    start = hash_function(iv.encode("utf-8"))
+    start = hash_function(bytes.fromhex(iv))
     t = hash_function(start)
     h = hash_function(hash_function(start))
-    while t != h:
-        t = hash_function(t)
-        h = hash_function(hash_function(h))
+    (t, h) = meeting_vertex(t, h, hash_function)
 
     # Tortoise and Hare met at some point.
     # Let the tortoise to go back to the starting point.
     # And they will move one edge per step.
     # The vertex they met is the initial edge that this cycle begins.
     t = start
-    while t != h:
-        t = hash_function(t)
-        h = hash_function(h)
+    (t, h) = initial_vertex(t, h, hash_function)
 
     # Fix the tortoise at that point, and move the hare to calculate the cycle length.
-    cycle_length = 1
     h = hash_function(t)
-    while t != h:
-        h = hash_function(h)
-        cycle_length += 1
+    (t, h, cycle_length) = calculate_cycle_length(t, h, hash_function)
+
+    delete_checkpoint(STEP1_COMPLETE_PKL)
+    delete_checkpoint(STEP2_COMPLETE_PKL)
 
     print(f"Initial Value: {t.hex()}, and the cycle length: {cycle_length}")
     print(f"Total seconds: {time.time() - start_time}")
+
+
+def meeting_vertex(
+    t: bytes, h: bytes, hash_function: Callable[[bytes], bytes]
+) -> Tuple[bytes, bytes]:
+    print("STEP 1: find meeting vertex")
+
+    try:
+        t, h, _ = load_checkpoint(STEP1_COMPLETE_PKL)
+        print(f"Step 1 Already Finished, t = {t.hex()}, h = {h.hex()}")
+        return (t, h)
+    except FileNotFoundError:
+        pass
+
+    count = 0
+
+    try:
+        t, h, count = load_checkpoint(STEP1_PKL)
+        print(f"Resumed from checkpoint: count = {count}")
+    except FileNotFoundError:
+        pass
+
+    while t != h:
+        count += 1
+        if count % CHECKPOINT == 0:
+            print(f"current count: {count}, t = {t.hex()}, h = {h.hex()}")
+            save_checkpoint(STEP1_PKL, t, h, count)
+
+        t = hash_function(t)
+        h = hash_function(hash_function(h))
+
+    print(f"STEP 1: finished. t = {t.hex()}, h = {h.hex()}")
+
+    delete_checkpoint(STEP1_PKL)
+    save_checkpoint(STEP1_COMPLETE_PKL, t, h, count)
+    return (t, h)
+
+
+def initial_vertex(
+    t: bytes, h: bytes, hash_function: Callable[[bytes], bytes]
+) -> Tuple[bytes, bytes]:
+    print("STEP 2: find initial vertex of the cycle")
+
+    try:
+        t, h, _ = load_checkpoint(STEP2_COMPLETE_PKL)
+        print(f"Step 2 Already Finished, t = {t.hex()}, h = {h.hex()}")
+        return (t, h)
+    except FileNotFoundError:
+        pass
+
+    count = 0
+
+    try:
+        t, h, count = load_checkpoint(STEP2_PKL)
+        print(f"Resumed from checkpoint: count = {count}")
+    except FileNotFoundError:
+        pass
+
+    while t != h:
+        count += 1
+        if count % CHECKPOINT == 0:
+            print(f"current count: {count}, t = {t.hex()}, h = {h.hex()}")
+            save_checkpoint(STEP2_PKL, t, h, count)
+
+        t = hash_function(t)
+        h = hash_function(h)
+
+    print(f"STEP 2: finished. t = {t.hex()}, h = {h.hex()}")
+
+    delete_checkpoint(STEP2_PKL)
+    save_checkpoint(STEP2_COMPLETE_PKL, t, h, count)
+    return (t, h)
+
+
+def calculate_cycle_length(
+    t: bytes, h: bytes, hash_function: Callable[[bytes], bytes]
+) -> Tuple[bytes, bytes, int]:
+    print("STEP 3: calculate length of the cycle")
+
+    cycle_length = 1
+
+    try:
+        t, h, cycle_length = load_checkpoint(STEP3_PKL)
+        print(f"Resumed from checkpoint: cycle_length = {cycle_length}")
+    except FileNotFoundError:
+        pass
+
+    while t != h:
+        if cycle_length % CHECKPOINT == 0:
+            print(f"cycle_length: {cycle_length}, t = {t.hex()}, h = {h.hex()}")
+            save_checkpoint(STEP3_PKL, t, h, count=cycle_length)
+
+        h = hash_function(h)
+        cycle_length += 1
+
+    print(f"STEP 3: finished. t = {t.hex()}, h = {h.hex()}, cycle_length = {cycle_length}")
+
+    delete_checkpoint(STEP3_PKL)
+    return (t, h, cycle_length)
